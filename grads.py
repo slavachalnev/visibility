8fba510470d7044a267a75d5378443ba8b90cb9f
Slava Chalnev <slava.chalnev@gmail.com>
2023-08-23 11:31:04 +1000

    Compute grads

--- grads.py
+++ grads.py
@@ -6,32 +6,29 @@ from transformer_lens import HookedTransformer
 
 def compute_grads(model: HookedTransformer, text, position, layer):
 
-    def require_grads(value, hook):
-        value = torch.tensor(value, requires_grad=True)
-        return value
-    
     cache = dict()
     def save_hook(value, hook):
+        value.retain_grad()
         cache[hook.name] = value
         return value
 
-    hooks = [(f'hook_embed', require_grads)]
-    for layer in range(model.cfg.n_layers):
-        hooks += [(f'blocks.{layer}.hook_resid_post', save_hook)]
+    hooks = []
+    for layer_idx in range(model.cfg.n_layers):
+        hooks += [(f'blocks.{layer_idx}.hook_resid_post', save_hook)]
 
     with model.hooks(fwd_hooks=hooks):
         model(text)
 
-    print(cache.keys())
     h = cache[f'blocks.{layer}.hook_resid_post'][0, position, :]
-    print(h.shape)
 
     # Compute gradients of h wrt every state that affects h.
-    h.sum().backward()
+    h.abs().sum().backward()
 
     grads_h_wrt_others = dict()
-
-
+    for layer_idx in range(layer): # 0, 1, ..., layer-1
+        grads_h_wrt_others[layer_idx] = cache[f'blocks.{layer_idx}.hook_resid_post'].grad
+        print(grads_h_wrt_others[layer_idx].shape)
+        print(grads_h_wrt_others[layer_idx])
 
 
 model = HookedTransformer.from_pretrained('gelu-4l', device='cpu')
@@ -40,15 +37,3 @@ text = "The cat sat on the mat."
 
 compute_grads(model, text, position=1, layer=1)
     
-
-
-
-
-
-
-
-
-
-
-
-
